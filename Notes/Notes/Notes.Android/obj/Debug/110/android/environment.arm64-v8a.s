def500f4cfb9905d7a4a20aaf5a38ccc891b96f1
Gioele <97604248+Giowis@users.noreply.github.com>
2022-02-09 14:05:18 +0100

    Nuovo Notes
    Veramente stavolta

--- Notes/Notes/Notes.Android/obj/Debug/110/android/environment.arm64-v8a.s
+++ Notes/Notes/Notes.Android/obj/Debug/110/android/environment.arm64-v8a.s
@@ -73,7 +73,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"9fe2d18e-bfe8-44b7-bff4-b7d95ee1eb8f"
+	.asciz	"afa82fc1-567e-43af-986c-086394234d09"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
